e7f6553ded56e7aadf9c225c5fa28e5e6a116bd1
André Cordeiro <am.cordeiro@campus.fct.unl.pt>
2023-03-27 12:45:29 +0100

    Updated Commit Message Class with given properties

--- src/consensus/messages/CommitMessage.java
+++ src/consensus/messages/CommitMessage.java
@@ -10,22 +10,33 @@ public class CommitMessage extends SignedProtoMessage{
 
     public final static short MESSAGE_ID = 103;	
 
-    public CommitMessage(){
+	public final int vN, sN, hashOpVal, iN;
+
+    public CommitMessage(int vN, int sN, int hashOpVal, int iN){
         super(CommitMessage.MESSAGE_ID);
+		this.vN = vN;
+		this.sN = sN;
+		this.hashOpVal = hashOpVal;
+		this.iN = iN;
     }
 
     public static final SignedMessageSerializer<CommitMessage> serializer = new SignedMessageSerializer<CommitMessage>() {
 
 		@Override
 		public void serializeBody(CommitMessage signedProtoMessage, ByteBuf out) throws IOException {
-			// TODO Auto-generated method stub, you should implement this method.
-			
+			out.writeInt(signedProtoMessage.vN);
+			out.writeInt(signedProtoMessage.sN);
+			out.writeInt(signedProtoMessage.hashOpVal);
+			out.writeInt(signedProtoMessage.iN);
 		}
 
 		@Override
 		public CommitMessage deserializeBody(ByteBuf in) throws IOException {
-			// TODO Auto-generated method stub, you should implement this method.
-			return null;
+			int vN = in.readInt();
+			int sN = in.readInt();
+			int hashOpVal = in.readInt();
+			int iN = in.readInt();
+			return new CommitMessage(vN, sN, hashOpVal, iN);
 		}
 		
 	};
