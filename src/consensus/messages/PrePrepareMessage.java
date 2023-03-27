1fc7a401f7f640628cc4571d3ece09db9b355e30
André Cordeiro <am.cordeiro@campus.fct.unl.pt>
2023-03-27 12:29:51 +0100

    Updated Preprepare messages with given properties
    To Check: Shuld the hash of the operation be an int?

--- src/consensus/messages/PrePrepareMessage.java
+++ src/consensus/messages/PrePrepareMessage.java
@@ -2,6 +2,8 @@ package consensus.messages;
 
 import java.io.IOException;
 
+import javax.swing.text.View;
+
 import io.netty.buffer.ByteBuf;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedMessageSerializer;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedProtoMessage;
@@ -9,25 +11,31 @@ import pt.unl.fct.di.novasys.babel.generic.signed.SignedProtoMessage;
 public class PrePrepareMessage extends SignedProtoMessage {
 
 	public final static short MESSAGE_ID = 101;
+
+	public final int vN, sN, hashOpVal;
 	
-	//TODO: Define here the elements of the message
-	
-	public PrePrepareMessage() {
+	public PrePrepareMessage(int vN, int sN, int hashOpVal) {  //TODO SHOULD THE HASH VALUE BE AN INT?
 		super(PrePrepareMessage.MESSAGE_ID);
+		this.vN = vN;
+		this.sN = sN;
+		this.hashOpVal = hashOpVal;
 	}
 
 	public static SignedMessageSerializer<PrePrepareMessage> serializer = new SignedMessageSerializer<PrePrepareMessage>() {
 
 		@Override
 		public void serializeBody(PrePrepareMessage signedProtoMessage, ByteBuf out) throws IOException {
-			// TODO Auto-generated method stub, you should implement this method.
-			
+			out.writeInt(signedProtoMessage.vN);
+			out.writeInt(signedProtoMessage.sN);
+			out.writeInt(signedProtoMessage.hashOpVal); 
 		}
 
 		@Override
 		public PrePrepareMessage deserializeBody(ByteBuf in) throws IOException {
-			// TODO Auto-generated method stub, you should implement this method.
-			return null;
+			int vN = in.readInt();
+			int sN = in.readInt();
+			int hashOpVal = in.readInt();
+			return new PrePrepareMessage(vN, sN, hashOpVal);
 		}
 		
 	};
