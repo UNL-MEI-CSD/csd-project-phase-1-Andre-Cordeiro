2103bda7a0362eb1acd4df1d141a4af53476cc20
Duda <m.contins@campus.fct.unl.pt>
2023-04-25 00:52:21 +0100

    Added some needed funcs/vars

--- src/consensus/messages/CommitMessage.java
+++ src/consensus/messages/CommitMessage.java
@@ -5,14 +5,17 @@ import java.io.IOException;
 import io.netty.buffer.ByteBuf;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedMessageSerializer;
 import pt.unl.fct.di.novasys.babel.generic.signed.SignedProtoMessage;
+import utils.SeqN;
 
 public class CommitMessage extends SignedProtoMessage{
 
     public final static short MESSAGE_ID = 103;	
 
-	public final int vN, sN, hashOpVal, iN;
+	public final int vN, hashOpVal, iN;
 
-    public CommitMessage(int vN, int sN, int hashOpVal, int iN){
+	public final SeqN sN;
+
+    public CommitMessage(int vN, SeqN sN, int hashOpVal, int iN){
         super(CommitMessage.MESSAGE_ID);
 		this.vN = vN;
 		this.sN = sN;
@@ -25,7 +28,7 @@ public class CommitMessage extends SignedProtoMessage{
 		@Override
 		public void serializeBody(CommitMessage signedProtoMessage, ByteBuf out) throws IOException {
 			out.writeInt(signedProtoMessage.vN);
-			out.writeInt(signedProtoMessage.sN);
+			signedProtoMessage.sN.serialize(out);
 			out.writeInt(signedProtoMessage.hashOpVal);
 			out.writeInt(signedProtoMessage.iN);
 		}
@@ -33,7 +36,7 @@ public class CommitMessage extends SignedProtoMessage{
 		@Override
 		public CommitMessage deserializeBody(ByteBuf in) throws IOException {
 			int vN = in.readInt();
-			int sN = in.readInt();
+			SeqN sN = SeqN.deserialize(in);
 			int hashOpVal = in.readInt();
 			int iN = in.readInt();
 			return new CommitMessage(vN, sN, hashOpVal, iN);
@@ -50,7 +53,7 @@ public class CommitMessage extends SignedProtoMessage{
 		return vN;
 	}
 
-	public int getsN() {
+	public SeqN getsN() {
 		return sN;
 	}
 
@@ -62,5 +65,16 @@ public class CommitMessage extends SignedProtoMessage{
 		return iN;
 	}
 
-	
+	public int getOp(){
+		return hashOpVal ;
+	}
+
+
+	public int getViewNumber() {
+		return vN;
+	}
+
+	public SeqN getSequenceNumber() {
+		return sN;
+	}
 }
